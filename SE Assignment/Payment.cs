c8f5e07bf3b5800200368edcdd6bb0aa70eb145d
keer1912 <S10194171@connect.np.edu.sg>
2023-02-03 01:34:15 +0800

    Added payment base class

---
+++ SE Assignment/Payment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+/// 
+public class Payment
+{
+    public Payment() { }
+
+        private string transactionId;
+
+        public string TransactionId   // property
+        {
+            get { return transactionId; }   // get method
+            set { transactionId = value; }  // set method
+        }
+
+        private double payableAmount;
+        public string PayableAmount   // property
+        {
+            get { return payableAmount; }   // get method
+            set { payableAmount = value; }  // set method
+        }
+
+        private string paymentMethod;
+        public string PaymentMethod   // property
+        {
+            get { return paymentMethod; }   // get method
+            set { paymentMethod = value; }  // set method
+        }
+
+        private string paymentStatus;
+        public string PaymentStatus   // property
+        {
+            get { return paymentStatus; }   // get method
+            set { paymentStatus = value; }  // set method
+        }
+}
