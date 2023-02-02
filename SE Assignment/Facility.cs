84e3184de6fce329a6d7a8fca406928b7a38a0fc
jarrellsoh <s10205047@connect.np.edu.sg>
2023-02-02 21:13:32 +0800

    Private variables for roomType and facility classes

---
+++ SE Assignment/Facility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Facility
+{
+    public Facility() { }
+
+    private int facilityId;
+    public int FacilityId
+    {
+        get { return facilityId; }
+        set { facilityId = value; }
+    }
+
+    private string facilityName;
+    public string FacilityName
+    {
+        get { return facilityName; }
+        set { facilityName = value; }
+    }
+}
+
