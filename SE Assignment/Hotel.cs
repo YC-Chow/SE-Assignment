3fa48f2ff97b76ea2d19a962e9e4d73f48c9b26a
Chow Yun Cong <S10194206@connect.np.edu.sg>
2023-02-02 21:30:22 +0800

    Add Hotel Class

---
+++ SE Assignment/Hotel.cs
@@ -0,0 +1,44 @@
+﻿
+namespace SE_Assignment {
+    public class Hotel {
+
+		private int hotelId;
+
+		public int HotelId {
+			get { return hotelId; }
+			set { hotelId = value; }
+		}
+
+		private string hotelName;
+
+		public string HotelName {
+			get { return hotelName; }
+			set { hotelName = value; }
+		}
+
+		private string hotelType;
+
+		public string HotelType {
+			get { return hotelType; }
+			set { hotelType = value; }
+		}
+
+		private bool hasVoucher;
+
+		public bool HasVoucher {
+			get { return hasVoucher; }
+			set { hasVoucher = value; }
+		}
+		
+
+		public Hotel() { }
+
+		public double getRating() {
+			return 0;
+		}
+
+		public int getNumOfRooms() {
+			return 0;
+		}
+    }
+}
