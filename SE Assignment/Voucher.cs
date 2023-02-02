f05716524b99c2207e06176bd94a281e722e26b3
keer1912 <S10194171@connect.np.edu.sg>
2023-02-03 01:34:27 +0800

    Added Voucher base class

---
+++ SE Assignment/Voucher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+/// 
+public class Payment
+{
+    public Payment() { }
+   
+    private int voucherId;
+    public int VoucherId   // property
+    {
+        get { return voucherId; }   // get method
+        set { voucherId = value; }  // set method
+    }
+
+    private string serialNo;
+    public string SerialNo   // property
+    {
+        get { return serialNo; }   // get method
+        set { serialNo = value; }  // set method
+    }
+
+    private string issuer;
+    public string Issuer   // property
+    {
+        get { return issuer; }   // get method
+        set { issuer = value; }  // set method
+    }
+
+    private DateTime expiryDate;
+    public DateTime ExpiryDate   // property
+    {
+        get { return expiryDate; }   // get method
+        set { expiryDate = value; }  // set method
+    }
+    private double voucherValue;
+    public double VoucherValue   // property
+    {
+        get { return voucherValue; }   // get method
+        set { voucherValue = value; }  // set method
+    }
+    private bool isPercentage;
+    public bool IsPercentage
+    {
+        get { return isPercentage; }
+        set { isPercentage = value; }
+    }
+    private bool isUsed;
+    public bool IsUsed
+    {
+        get { return isUsed; }
+        set { isUsed = value; }
+    }
+}
