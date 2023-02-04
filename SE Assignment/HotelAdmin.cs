a3b2d697aec5d82c23902f57a5d1f6f79bf65417
reathanluo <s10205205@connect.np.edu.sg>
2023-02-04 11:52:08 +0800

    update HotelAdmin

---
+++ SE Assignment/HotelAdmin.cs
@@ -0,0 +1,40 @@
+﻿public class HotelAdmin
+{
+    public HotelAdmin() { }
+
+    private string name;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public string EmailAddress
+    {
+        get { return emailAddress; }
+        set { emailAddress = value; }
+    }
+
+    private Hotel managedHotel;
+
+    public Hotel ManagedHotel
+    {
+        set
+        {
+            if (managedHotel != value)
+            {
+                managedHotel = value;
+                value.addHotel(this);
+            }
+        }
+    }
+
+    public Reservation BookHotelRoom(DateTime checkInDate, DateTime checkInDate, int roomId, int guestId )
+    {
+
+        return Reservation;
+    }
+
+
+}
+
