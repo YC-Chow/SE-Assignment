289eec3d49854b5e82d7f0ab79bf806771326895
Chow Yun Cong <S10194206@connect.np.edu.sg>
2023-02-07 19:25:54 +0800

    Update Iterator and Collection class for reservation

--- SE Assignment/Iterator/ReservationCollection.cs
+++ SE Assignment/Iterator/ReservationCollection.cs
@@ -1,10 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SE_Assignment.Iterator {
+    public class ReservationCollection {
+        private List<Reservation> reservations = new List<Reservation>();
 
-namespace SE_Assignment.Iterator {
-    internal class ReservationCollection {
+        public ReservationIterator createIterator() {
+            return new ReservationIterator(this);
+        }
+
+        public int Count {
+            get { return reservations.Count; }
+        }
+
+        public void Add(Reservation reservation) {
+            reservations.Add(reservation);
+        }
+
+        public void Remove(Reservation reservation) {
+            reservations.Remove(reservation);
+        }
+
+        public Reservation GetReservation(int index) {
+            return reservations[index];
+        }
     }
 }
--- SE Assignment/Iterator/ReservationIterator.cs
+++ SE Assignment/Iterator/ReservationIterator.cs
@@ -1,8 +1,33 @@
 ﻿
 namespace SE_Assignment.Iterator {
-    interface ReservationIterator {
-        Reservation First();
-        Reservation Next();
-        bool IsCompleted();
+    public class ReservationIterator {
+        private ReservationCollection collection;
+        private int current = 0;
+        private int step = 1;
+
+        public ReservationIterator(ReservationCollection reservations) { 
+            this.collection = reservations;
+        }
+
+        public Reservation First() {
+            current = 0;
+            return collection.GetReservation(current);
+        }
+
+        public Reservation Next() {
+            current += 1;
+            if (!IsCompleted) {
+                return collection.GetReservation(current);
+            }
+            else {
+                return null;
+            }
+        }
+
+        public bool IsCompleted {
+            get { return current >= collection.Count; }
+        }
+
+      
     }
 }
