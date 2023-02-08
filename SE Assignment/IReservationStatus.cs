8da522e926e39aa9bf1ead89391967ef64be1492
mahshuk410 <s10204884@connect.np.edu.sg>
2023-02-08 22:34:07 +0800

    create interface class for ReservationStatus

---
+++ SE Assignment/IReservationStatus.cs
@@ -0,0 +1,112 @@
+﻿
+
+namespace SE_Assignment
+{
+     interface IReservationStatus
+    {
+        public void makeReservation(DateTime checkInDate, DateTime checkOutDate, List<int> RoomTypeIdList, Guest guest);
+        public void cancelReservation(Reservation reservation);
+
+        //public void makePayment(Double amount, int reservationId, string paymentMethod);
+        public void guestCheckIn(Reservation reservation);
+    }
+    class SubmittedState : IReservationStatus
+    {
+        public void makeReservation(DateTime checkInDate, DateTime checkOutDate, List<int> RoomTypeIdList, Guest guest)
+        {
+            Reservation r = new Reservation(checkInDate, checkOutDate);
+            r.ReservedByGuest = guest;
+            r.ReservationStatus = new ConfirmedState(); 
+            r.ReservationDate = DateTime.Today;
+            r.ReservationId = 0;
+            guest.addReservation(r);
+        }
+        public void guestCheckIn(Reservation r) {
+            Console.WriteLine("Payment has not been made yet. Please make payment to confirm this booking.");
+        }
+        public void cancelReservation(Reservation reservation)
+        {
+            Console.WriteLine("This booking has been cancelled successfully. No payment amount has been deducted.");
+            reservation.ReservationStatus = new CancelledState();
+
+        }
+    }
+    class ConfirmedState : IReservationStatus
+    {
+        public void makeReservation(DateTime checkInDate, DateTime checkOutDate, List<int> RoomTypeIdList, Guest guest)
+        {
+            Console.WriteLine("You have an existing reservation confirmed! You can't rebook the same reservation.");
+        }
+        public void guestCheckIn(Reservation reservation)
+        {
+            if (DateTime.Now == reservation.CheckInDate && DateTime.Now < new DateTime(DateTime.Now.Year,DateTime.Now.Month,DateTime.Now.Day,23,59,0) && DateTime.Now.Hour >= 14)//ontime
+            {
+                reservation.ReservationStatus = new FulfilledState();
+                Console.WriteLine("Successfully checked-in to hotel.");
+
+            }
+            else
+            {
+                reservation.ReservationStatus = new NoShowState();
+                Console.WriteLine("You have exceeded the grace period for checking-in. Sorry, check-in not allowed.");
+
+            }
+
+        }
+        public void cancelReservation(Reservation reservation)
+        {
+            Console.WriteLine("This booking has been cancelled successfully. Payment made will be refunded to you.");
+            reservation.ReservationStatus = new CancelledState();
+
+        }
+    }
+    class FulfilledState : IReservationStatus
+    {
+        public void makeReservation(DateTime checkInDate, DateTime checkOutDate, List<int> RoomTypeIdList, Guest guest)
+        {
+            Console.WriteLine("Error. An existing reservation with the similarly specified reservation details has been fulfilled");
+        }
+        public void guestCheckIn(Reservation reservation)
+        {
+            Console.WriteLine("Error. This reservation has been fulfilled and guests have checked out.");
+        }
+        public void cancelReservation(Reservation reservation)
+        {
+            Console.WriteLine("This booking is ongoing as guests have already checked-in");
+        }
+
+
+
+    }
+    class NoShowState : IReservationStatus
+    {
+        public void makeReservation(DateTime checkInDate, DateTime checkOutDate, List<int> RoomTypeIdList, Guest guest) {
+            Console.WriteLine("Error. This existing reservation has been marked as no-show");
+        }
+        public void guestCheckIn(Reservation reservation)
+        {
+            Console.WriteLine("You have exceeded the grace period for checking-in. Sorry, check-in not allowed.");
+        }
+        public void cancelReservation(Reservation reservation)
+        {
+            Console.WriteLine("This booking has already been paid, and cannot be refunded as user failed to check-in ontime.");
+        }
+
+    }
+    class CancelledState : IReservationStatus
+    {
+        public void makeReservation(DateTime checkInDate, DateTime checkOutDate, List<int> RoomTypeIdList, Guest guest)
+        {
+            Console.WriteLine("This reservation has been cancelled.");
+        }
+        public void guestCheckIn(Reservation reservation)
+        {
+            Console.WriteLine("This booking has been cancelled. No check-ins allowed.");
+        }
+        public void cancelReservation(Reservation reservation)
+        {
+            Console.WriteLine("This booking has already been cancelled.");
+        }
+
+    }
+}
--- SE Assignment/Reservation.cs
+++ SE Assignment/Reservation.cs
@@ -4,13 +4,7 @@ public class Reservation
 {
 
 
-	public enum Status {
-		SUBMITTED, //0
-		CONFIRMED, //1
-		FULFILLED, //2
-		CANCELLED, //3
-		NO_SHOW //4
-	}
+	
 	public Reservation(DateTime checkInDate,DateTime checkOutDate)//constructor
 	{
 		this.checkInDate = checkInDate;
@@ -41,12 +35,7 @@ public class Reservation
 		get { return checkOutDate; }
 		set { checkOutDate = value; }
 	}
-	private Status reservationStatus;
-	public Status ReservationStatus
-	{
-		get { return reservationStatus; }
-		set { reservationStatus = value; }
-	}
+	public IReservationStatus ReservationStatus;
 	private Guest reservedByguest;
 	public Guest ReservedByGuest
 	{
@@ -92,80 +81,5 @@ public class Reservation
         }
     }
 	
-	public void makeReservation(DateTime checkInDate, DateTime checkOutDate,List<int> RoomTypeIdList,Guest guest)
-    {
-		
-		Reservation r = new Reservation(checkInDate,checkOutDate);
-		r.ReservedByGuest = guest;
-		
-		if (r.reservationStatus == null)
-        {
-			r.reservationStatus = Status.SUBMITTED;
-			r.reservationDate = DateTime.Today;
-			
-			r.ReservationId = 0;
-			guest.addReservation(r);
-		}
-		else if (r.reservationStatus == Status.CONFIRMED)
-        {
-			Console.WriteLine("You have an existing reservation confirmed! You can't rebook the same reservation.");
-        }else if (r.reservationStatus == Status.CANCELLED)
-        {
-			Console.WriteLine("This reservation has been cancelled.");
-        }else if (r.reservationStatus == Status.NO_SHOW)
-        {
-			Console.WriteLine("Error. This existing reservation has been marked as no-show");
-        }
-		
-
-    }
-	public void GuestCheckIn(int reservationId)
-    {
-		if (reservationStatus == Status.SUBMITTED){
-			Console.WriteLine("Payment has not been made yet. Please make payment to confirm this booking.");
-        }else if(reservationStatus == Status.CANCELLED)
-        {
-			Console.WriteLine("This booking has been cancelled. No check-ins allowed.");
-        }else if (reservationStatus == Status.NO_SHOW)
-        {
-			Console.WriteLine("You have exceeded the grace period for checking-in. Sorry, check-in not allowed.");
-        }else if (reservationStatus == Status.CONFIRMED)
-        {
-			if (DateTime.Now == checkInDate && DateTime.Now.Hour < 23 && DateTime.Now.Minute >= 14)//ontime
-            {
-				reservationStatus = Status.FULFILLED;
-				Console.WriteLine("Successfully checked-in to hotel.");
-
-			}
-			else
-            {
-				reservationStatus = Status.NO_SHOW;
-				Console.WriteLine("You have exceeded the grace period for checking-in. Sorry, check-in not allowed.");
-
-			}
-		}
-    }
-	public cancelReservation(int ReservationId)
-    {
-		if (this.reservationStatus == Status.CANCELLED)
-        {
-			Console.WriteLine("This booking has already been cancelled.");
-        }else if(this.reservationStatus == Status.FULFILLED)
-        {
-			Console.WriteLine("This booking is ongoing as guests have already checked-in");
-        }else if (this.reservationStatus == Status.NO_SHOW)
-        {
-			Console.WriteLine("This booking has already been paid, and cannot be refunded as user failed to check-in ontime.");
-        }else if (this.reservationStatus == Status.CONFIRMED)
-        {
-			Console.WriteLine("This booking has been cancelled successfully. Payment made will be refunded to you.");
-			this.reservationStatus = Status.CANCELLED;
-			
-        }else if (this.reservationStatus == Status.SUBMITTED)
-        {
-			Console.WriteLine("This booking will has been cancelled successfully. No payment amount has been deducted.");
-			this.reservationStatus = Status.CANCELLED;
-
-        }
-    }
+	
 }
