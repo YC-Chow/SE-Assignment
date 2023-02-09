273b2cd9f26f32a6bf3eb246b41bfd7e15343e42
keer1912 <S10194171@connect.np.edu.sg>
2023-02-09 22:19:50 +0800

    Added Voucher Collection

---
+++ SE Assignment/Iterator/VoucherCollection.cs
@@ -0,0 +1,19 @@
+﻿namespace SE_Assignment.Iterator
+{
+    public interface IVoucherCollection
+    {
+        VoucherIterator CreateIterator();
+    }
+
+    public class VoucherCollection : List<Voucher>, IVoucherCollection
+    {
+        public VoucherIterator CreateIterator()
+        {
+            return new VoucherIterator(this);
+        }
+        public int count
+        {
+            get { return this.Count; }
+        }
+    }
+}
